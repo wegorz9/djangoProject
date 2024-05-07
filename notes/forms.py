b2f1e43b48ab545b0e6612a46e9d2b04a48553a7
wegorz9 <rpawer2@gmail.com>
2024-05-07 14:39:09 +0200

    Added basic views and some functionality

---
+++ notes/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Note
+
+class NoteForm(forms.ModelForm):
+    class Meta:
+        model = Note
+        fields = ['title', 'content', 'priority', 'author']
+        widgets = {
+            'author': forms.HiddenInput(),
+        }
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
